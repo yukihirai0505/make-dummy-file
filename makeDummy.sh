37329920b8319211f65f4c9fe051be27d9fa10c1
Yuki Hirai <yukihirai0505@gmail.com>
2017-04-17 12:14:31 +0900

    Add makeDummy sh

---
+++ makeDummy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget http://lorempixel.com/200/200/ -O dummy.jpeg
+cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 1048218 | head -n 1 > dummy.txt
+exiftool dummy.jpeg -comment\<=dummy.txt
+rm dummy.jpeg_original
+rm dummy.txt
+
+
