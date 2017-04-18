b94bb1d0e54e8b6d9d07e3198fd06d574f1d9a99
Yuki Hirai <yukihirai0505@gmail.com>
2017-04-18 09:57:30 +0900

    Add size option

--- makeDummy.sh
+++ makeDummy.sh
@@ -4,8 +4,9 @@ echo args=$@
 
 HEIGHT=200
 WIDTH=200
+SIZE=1048218
 
-while getopts ":h:w:" OPT ; do
+while getopts ":h:w:s:" OPT ; do
   case $OPT in
     # HEIGHT
     h)
@@ -19,6 +20,12 @@ while getopts ":h:w:" OPT ; do
       echo $OPTARG
       WIDTH=$OPTARG
       ;;
+    # SIZE
+    s)
+      echo $OPT
+      echo $OPTARG
+      SIZE=$OPTARG
+      ;;
    esac
 done
 
@@ -26,7 +33,7 @@ done
 wget http://placehold.jp/${HEIGHT}x${WIDTH}.png -O dummy.png
 
 # Make dummy text
-cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 1048218 | head -n 1 > dummy.txt
+cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w ${SIZE} | head -n 1 > dummy.txt
 
 # Insert comment to image
 exiftool dummy.png -comment\<=dummy.txt
